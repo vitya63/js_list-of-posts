266c4cab89cafb9c11bbdc5eb624dce843e6baa2
Viktor Lukovets <nuerio63@gmail.com>
2019-10-24 17:07:04 +0300

    solution;

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# List of posts
+# External Gulp Files
 
-By requesting https://jsonplaceholder.typicode.com/posts, https://jsonplaceholder.typicode.com/users, and https://jsonplaceholder.typicode.com/comments, create and fill a list of posts with the following data on each:
-
-1) the title of the post;
-2) the text (body) of the post;
-3) the name of the author of the post;
-4) sublist of the post comments, including the name of the author of each comment as well as the comment itself.
-
- - [DEMO LINK](https://vitya63.github.io/js_list-of-posts/)
+This shows how to import [Semantic UI](https://github.com/Semantic-Org/Semantic-UI/) build tasks into your own `gulpfile.js`
--- scripts/main.js
+++ scripts/main.js
@@ -12,21 +12,24 @@ async function createList() {
   const posts = await loadData('https://jsonplaceholder.typicode.com/posts');
   const users = await loadData('https://jsonplaceholder.typicode.com/users');
   const comments = await loadData('https://jsonplaceholder.typicode.com/comments');
+
   for (let i = 0; i < posts.length; i++) {
     const li = document.createElement('LI');
     const currentUser = users.find(user => user.id == posts[i].userId);
     const relevantComments = comments.filter(comment => comment.postId == posts[i].id);
+
     li.innerHTML = `
       <hr>
       <h1>${posts[i].title}</h1>
       <p>${posts[i].body}</p>
       <div><i>${currentUser.name}</i></div>
-      <button style="margin-top: 10px" class="button">Show comments</button>
-      <ul id="nestedUl" class="nestedUl" hidden></ul>
+      <button style="margin-top: 10px" id="button">Show comments</button>
+      <ul id="nestedUl" hidden></ul>
       <hr>
     `;
     list.append(li);
     let currentPost = document.querySelector('#nestedUl');
+
     for (const comment of relevantComments) {
       const li = document.createElement('LI');
       li.innerHTML = `
@@ -35,23 +38,22 @@ async function createList() {
       `;
       currentPost.append(li);
     }
+
     currentPost.id = '';
   }
 }
 
 createList()
   .then(() => {
-    const buttons = document.querySelectorAll('.button');
-    const nestedLists = document.querySelectorAll('.nestedUl');
+    const buttons = document.querySelectorAll('#button');
+
     for (const button of buttons) {
       button.addEventListener('click', (event) => {
-        if (event.target.textContent == 'Show comments') {
-          console.log('show');
+        if (event.target.textContent === 'Show comments') {
           event.target.textContent = 'Hide comments';
           event.target.nextElementSibling.hidden = false;
           return;
         } else if (event.target.textContent == 'Hide comments') {
-          console.log('hide');
           event.target.textContent = 'Show comments';
           event.target.nextElementSibling.hidden = true;
         }
